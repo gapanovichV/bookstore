4284c36fe1dd3469579b67fca6151a1fbc872d3d
Vlad Gapanovich <vladgapanovich172@gmail.com>
2023-12-10 20:33:14 +0300

    Discover page add card

--- src/features/Discover/Content/ui/Content.tsx
+++ src/features/Discover/Content/ui/Content.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import cn from "classnames";
 import cls from './Content.module.scss'
+import {Card, SizeCard} from "entities/Card";
 
 interface ContentProps {
     className?: string
@@ -8,7 +9,24 @@ interface ContentProps {
 export const Content  = ({className}: ContentProps) => {
   return (
     <div className={cn(cls.content, className)}>
-        Content
+      <div className={cn(cls.header)}>
+        Showing 1 - 9 items out of a total of ? million books
+        <div className={cn(cls.dropdown)}>Sort by:  DROPDOWN </div>
+      </div>
+      <div className={cn(cls.book_list)}>
+        <Card sizeCard={SizeCard.LARGE}/>
+        <Card sizeCard={SizeCard.LARGE}/>
+        <Card sizeCard={SizeCard.LARGE}/>
+        <Card sizeCard={SizeCard.LARGE}/>
+        <Card sizeCard={SizeCard.LARGE}/>
+        <Card sizeCard={SizeCard.LARGE}/>
+        <Card sizeCard={SizeCard.LARGE}/>
+        <Card sizeCard={SizeCard.LARGE}/>
+        <Card sizeCard={SizeCard.LARGE}/>
+      </div>
+      <div className={cn(cls.pagination)}>
+        PAGINATION
+      </div>
     </div>
   );
 }
