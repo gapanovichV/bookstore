a95fe3c0db0ed63ca3809f6701be528258dd581c
gapanovichV <vladgapanovich172@gmail.com>
2024-04-05 23:44:49 +0300

    sort new and all books

--- src/entities/AllBook/index.ts
+++ src/entities/AllBook/index.ts
@@ -1,5 +1,5 @@
 import type {IAllBookSchema, BookSchemaApi} from './model/types/allBookTypes'
-import {allBookReducer, allBookActions, fetchAllBook} from "./model/slice/allBookSlice";
+import {allBookReducer, allBookActions, fetchAllBook, AllBookSlice} from "./model/slice/allBookSlice";
 import {getAllBook} from "./model/selectors/getAllBook";
 
-export {type IAllBookSchema, allBookActions, allBookReducer, getAllBook, fetchAllBook,  type BookSchemaApi}
+export {type IAllBookSchema, allBookActions, allBookReducer, getAllBook, fetchAllBook, AllBookSlice,  type BookSchemaApi}
